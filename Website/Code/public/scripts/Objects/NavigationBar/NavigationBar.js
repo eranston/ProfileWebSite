28b2e3923c0901c7625c42e30bec14a3f6355377
Eran Ston <1eranston1@gmail.com>
2025-05-03 17:04:39 +0300

    removed unnecessery dic

--- Website/Code/public/scripts/Objects/NavigationBar/NavigationBar.js
+++
@@ -1,7 +0,0 @@
-export function CreateNavigationBar() {
-    return '<header class="top-nav">\
-    <button class="home-button" onclick="location.href="Homepage"">HomePage</button>\
-    <button class="timeline-button" onclick="location.href="Timeline"">Timeline</button>\
-    <button class="about-button" onclick="location.href="About"">About</button>\
-    </header>';
-}
